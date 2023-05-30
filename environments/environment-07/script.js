3e63f1593c6e7060e06ae6a0b90b5bb1d572a583
Jasa95 <jaco3020@stud.kea.dk>
2023-05-30 12:36:55 +0200

    øvelse 3 environment 7

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,4 +1,26 @@
 "use strict";
 
-window.addEventListener("load", start)
+window.addEventListener("load", start);
 
+let studentList = [];
+
+function start() {
+    document.querySelector("#create-student-form").addEventListener("submit", createStudentClicked);
+};
+
+function createStudent(name, email, age) {
+    const student = {name: name, email: email, age: age}
+    studentList.push(student)
+    console.log(student)
+};
+
+function createStudentClicked(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const age = form.age.value;
+
+    createStudent(name, email, age)
+}
