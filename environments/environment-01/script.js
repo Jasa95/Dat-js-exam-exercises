4a69571657700fb825b65ded46cb1d5bf9623e52
Jasa95 <jaco3020@stud.kea.dk>
2023-06-11 16:14:31 +0200

    rettelser af main

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,16 +1,1 @@
 "use strict";
-
-window.addEventListener("load", start);
-
-async function start() {
-    const users = await getData()
-    console.log(users);
-};
-
-async function getData() {
-    const response = await fetch("users.json");
-    const data = await response.json();
-
-    return data;
-  
-};
