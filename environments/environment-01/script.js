6bacbf4718868038f32b0a8fe5501f74e0d77e49
Jasa95 <jaco3020@stud.kea.dk>
2023-06-11 16:14:04 +0200

    løst uden kommentare

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,16 +1,53 @@
 "use strict";
 
-window.addEventListener("load", start);
+window.addEventListener("load", initApp);
 
-async function start() {
-    
-    console.log(users);
+// lavet en variable til et tomt array
+let userArray = [];
+
+
+
+async function initApp() {
+    // smider users in i det tomme array
+    await getData();
+    console.log(userArray);
+    showUsers();
 };
 
+// laver asynkron funktion da vi skal bruge await
 async function getData() {
+    // sender request til serveren og venter på svar 
     const response = await fetch("users.json");
     const users = await response.json();
 
-    return users
-  
-};
+    prepareUsers(users)
+
+    function prepareUsers(users) {
+    for(const key in users){
+        const userobject = users[key]
+        userArray.push(userobject)
+    }
+    };
+}
+
+function displayUsers(user) {
+    
+    const userHTML = /*HTML*/`
+    
+    <li>Navn: ${user.name}</li>
+    <li>Active: ${user.active}</li>
+    `;
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", userHTML);
+;}
+
+function showUsers(){
+    userArray.forEach(displayUsers)
+
+    for (const user of userArray){
+        if(user.role === "admin"){
+            document.querySelector("#userlist").insertAdjacentHTML("beforeend", /*HTML */`
+            <li><p>Navn: ${user.name} ▪︎ ${user.active}</p></li>
+            `);
+        }
+    }
+}
