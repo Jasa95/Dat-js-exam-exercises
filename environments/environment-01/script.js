15a1c5669a5fa6a0dbad043fea4f3ea2d0693cac
Jasa95 <jaco3020@stud.kea.dk>
2023-06-11 19:39:24 +0200

    smart løst

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -2,52 +2,27 @@
 
 window.addEventListener("load", initApp);
 
-// lavet en variable til et tomt array
-let userArray = [];
-
-
-
 async function initApp() {
-    // smider users in i det tomme array
-    await getData();
-    console.log(userArray);
-    showUsers();
-};
+  const users = await getData();
 
-// laver asynkron funktion da vi skal bruge await
-async function getData() {
-    // sender request til serveren og venter på svar 
-    const response = await fetch("users.json");
-    const users = await response.json();
-
-    prepareUsers(users)
-
-    function prepareUsers(users) {
-    for(const key in users){
-        const userobject = users[key]
-        userArray.push(userobject)
-    }
-    };
+  showUsers(users);
 }
 
-function displayUsers(user) {
-    
-    const userHTML = /*HTML*/`
-    
-    <li>Navn: ${user.name}</li>
-    <li>Active: ${user.active}</li>
+async function getData() {
+  const res = await fetch("users.json");
+  return res.json();
+}
+
+function showUsers(users) {
+  console.log(users);
+  const list = document.querySelector("#userlist");
+
+  const filter = users.filter((user) => user.role === "admin");
+
+  for (const user of filter) {
+    const html = /*html*/ `
+    <li>name: ${user.name} - active: ${user.active}</li>
     `;
-    document.querySelector("#userlist").insertAdjacentHTML("beforeend", userHTML);
-;}
-
-function showUsers(){
-    userArray.forEach(displayUsers)
-
-    for (const user of userArray){
-        if(user.role === "admin"){
-            document.querySelector("#userlist").insertAdjacentHTML("beforeend", /*HTML */`
-            <li><p>Navn: ${user.name} ▪︎ ${user.active}</p></li>
-            `);
-        }
-    }
+    list.insertAdjacentHTML("beforeend", html);
+  }
 }
