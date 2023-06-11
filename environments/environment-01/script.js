a64b57c8729d5a691756cfd4db582689f7080def
Jasa95 <jaco3020@stud.kea.dk>
2023-06-11 12:05:20 +0200

    start op

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -3,14 +3,14 @@
 window.addEventListener("load", start);
 
 async function start() {
-    const users = await getData()
+    
     console.log(users);
 };
 
 async function getData() {
     const response = await fetch("users.json");
-    const data = await response.json();
+    const users = await response.json();
 
-    return data;
+    return users
   
 };
