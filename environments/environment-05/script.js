132be48b3ce834d367437926eb647829e3364a26
Jasa95 <jaco3020@stud.kea.dk>
2023-06-12 16:22:35 +0200

    Løst

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log(courses);
+  sortCourse();
+  showCourse();
+}
+
+function showCourse() {
+  const list = document.querySelector("#courses-list");
+
+  for (const course of courses) {
+    const html = /*html*/ `
+        <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
+        `;
+    list.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function sortCourse() {
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
