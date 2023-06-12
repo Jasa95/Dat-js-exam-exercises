b9beb26a2aa977f8e61485162954c15d626ca450
Jasa95 <jaco3020@stud.kea.dk>
2023-06-12 10:37:11 +0200

    start

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// window med eventlistner load og funktionsreference
+window.addEventListener("load", start);
+// laver en global variable med et tomt array
+let users = [];
+// reference funktionen som er asyn da vi har med fetch og await at gøre
+async function start() {
+  // sætter vores getData funktion ind i vores globale variable for at få vores data ind i arrayet
+  users = await getData();
+  //tjekker om vi har vores data i vores globale variabel
+  console.log(users);
+}
+
+// asyn funktion der henter json filen og gemmer den i en constant som oversættes til js
+async function getData() {
+  const res = await fetch("users.json");
+  return res.json();
+}
