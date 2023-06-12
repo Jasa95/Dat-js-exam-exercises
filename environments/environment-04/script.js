9f165d4801127024b13d69eba8b3138ad7f02035
Jasa95 <jaco3020@stud.kea.dk>
2023-06-12 15:39:44 +0200

    Fuldført!

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,28 @@
 "use strict";
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  showTeachers();
+  createTeacher("lars", "larsdsgs");
+}
+
+function showTeachers() {
+  const list = document.querySelector("#teachers-list");
+  list.innerHTML = "";
+
+  for (const teacher of teachers) {
+    const html = /*html*/ `
+        <li>Name: ${teacher.name} - ${teacher.email}</li>
+        `;
+    list.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function createTeacher(name, email) {
+  const newTeacher = { name, email };
+
+  teachers.push(newTeacher);
+  showTeachers();
+}
