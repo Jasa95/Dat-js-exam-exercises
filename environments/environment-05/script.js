86975cefa4fc6340c96d1431fdd5ab51cbeb5a1f
Jasa95 <jaco3020@stud.kea.dk>
2023-06-13 12:53:51 +0200

    Update script.js

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -6,22 +6,25 @@ window.addEventListener("load", start);
 
 function start() {
   console.log(courses);
+  showCourse()
   filterCourse();
-  showCourse();
+  
 
   document
     .querySelector("#select-filter-ects")
     .addEventListener("change", () => {
-      const filterBy = document.querySelector("#select-filter-ects").valueOf;
+      const filterBy = document.querySelector("#select-filter-ects").value;
       filterCourse(filterBy);
     });
 }
 
 function showCourse() {
+    
   const list = document.querySelector("#courses-list");
   list.innerHTML = "";
 
   for (const course of courses) {
+    
     const html = /*html*/ `
       <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
       `;
@@ -29,10 +32,17 @@ function showCourse() {
   }
 }
 /// forkert damnit
-function filterCourse(filterBy) {
-  console.log(filterBy);
-  if (filterBy === "value") {
-    courses.filter((a, b) => a[filterBy].localeCompare(b[filterBy]));
-  } else courses.filter((a, b) => a[filterBy] - b[filterBy]);
-  showCourse();
+function filterCourse(filterBy){
+     const list = document.querySelector("#courses-list");
+     list.innerHTML = "";
+
+     for (const course of courses) {
+       if (course.ectsPoints.toString() == filterBy) {
+         const html = /*html*/ `
+        <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
+        `;
+         list.insertAdjacentHTML("beforeend", html);
+       }
+     }
+
 }
