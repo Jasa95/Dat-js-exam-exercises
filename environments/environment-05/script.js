0b893ca9c49613f22fc437bcbaf1580ca1298293
Jasa95 <jaco3020@stud.kea.dk>
2023-06-13 12:27:21 +0200

    3 svær

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -5,19 +5,27 @@ import { courses } from "./courses.js";
 window.addEventListener("load", start);
 
 function start() {
-    console.log(courses);
-    showCourse();
+  console.log(courses);
+  
+  showCourse()
+
+  document.querySelector("#select-filter-ects").addEventListener("change", filterCourse);
 }
 
 function showCourse() {
-    const list = document.querySelector("#courses-list");
-
-    for(const course of courses) {
-        const html = /*html*/ `
-        <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
-        `
-        list.insertAdjacentHTML("beforeend",html)
+  const list = document.querySelector("#courses-list");
+  list.innerHTML=""
+  
+  
+  for (const course of courses) {
+      const html = /*html*/ `
+      <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
+      `;
+      list.insertAdjacentHTML("beforeend", html);
     }
-
+    
 }
 
+function filterCourse() {
+   
+}
