0fc7d1581edf63b75ae43ae8ec8d68a9827eacca
Jasa95 <jaco3020@stud.kea.dk>
2023-06-13 13:08:50 +0200

    done

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,26 @@
 "use strict";
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log(courses);
+  sortCourse();
+}
+
+function showCourse() {
+  const list = document.querySelector("#courses-list");
+  list.innerHTML = "";
+
+  for (const course of courses) {
+    const html = /*html */ `
+        <li>Name: ${course.name} - startDate: ${course.startDate} - ectsPoints: ${course.ectsPoints}</li>
+        `;
+    list.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function sortCourse() {
+  courses.sort((a, b) => a.startDate.localeCompare(b.startDate));
+  showCourse();
+}
