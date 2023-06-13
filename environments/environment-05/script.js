45b77d4618791d26bdf9c62875e7fa93205590cf
Jasa95 <jaco3020@stud.kea.dk>
2023-06-13 12:13:28 +0200

    del 1 og 2

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log(courses);
+    showCourse();
+}
+
+function showCourse() {
+    const list = document.querySelector("#courses-list");
+
+    for(const course of courses) {
+        const html = /*html*/ `
+        <li>Name: ${course.name} - ectsPoints: ${course.ectsPoints}</li>
+        `
+        list.insertAdjacentHTML("beforeend",html)
+    }
+
+}
+
